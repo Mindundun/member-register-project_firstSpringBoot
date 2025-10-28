602b2335480c1e6025e0caecafed81f4bd267ab8
Mindundun <pmk10077@gmail.com>
2025-10-28 13:38:13 +0900

    Spring JdbcTemplate

--- src/main/java/com/example/member_register_project/config/SpringConfig.java
+++ src/main/java/com/example/member_register_project/config/SpringConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.member_register_project.repository.MemberRepository;
 import com.example.member_register_project.repository.MemoryMemberRepository;
-import com.example.member_register_project.repository.jdbcMemberRepository;
+import com.example.member_register_project.repository.JdbcMemberRepository;
 import com.example.member_register_project.service.MemberService;
 
 @Configuration
@@ -28,6 +28,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new jdbcMemberRepository(dataSource);
+        return new JdbcMemberRepository(dataSource);
     }
 }
